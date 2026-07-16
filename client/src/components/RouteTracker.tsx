af212c7ae726371633c3f32762b8ee4cd7bab05b
Strebulaev <strebulasergey@gmail.com>
2026-07-16 23:28:48 +0300

    fix: fix routing and search - Changed project-:id to project/:id - Changed task-:id to task/:id - Changed profile-:id to profile/:id - Fixed redirect after login to preserve last visited route - Added useDebounce hook for search input - Added 401 interceptor for automatic redirect to login

---
+++ client/src/components/RouteTracker.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export function RouteTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    localStorage.setItem('lastRoute', location.pathname);
+  }, [location]);
+
+  return null;
+}
